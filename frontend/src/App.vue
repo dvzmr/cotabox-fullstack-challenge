59b2418193845110982decf60b692d3205c82e5a
David Souza de Moura <mourazdavid@gmail.com>
2024-02-20 04:22:37 -0300

    folder refactor + add base frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <ParticipantForm/>
+</template>
+<script>
+import ParticipantForm from "@/components/SendParticipantForm.vue";
+
+export default {
+  name: 'App',
+  components: {
+    ParticipantForm
+  }
+}
+</script>
+<style>
+.logo {
+  width: 40rem;
+}
+
+.register input {
+  width: 25ch;
+  min-height: 2rem;
+  padding-left: 0.5rem;
+  margin-right: 1rem;
+}
+
+.register #sendButton {
+  width: 10ch;
+  min-height: 2rem;
+  background-color: deepskyblue;
+  color: white;
+  border-color: white;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/src/components/SendParticipantForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="register">
+    <form @submit="onSubmit">
+      <input type="text" v-model="input_first_name" placeholder="First Name" >
+      <input type="text" v-model="input_last_name" placeholder="Last Name">
+      <input type=number v-model="input_participation" placeholder="Participation">
+      <input type="submit" id="sendButton" value="SEND" v-on:click="registerParticipant" >
+    </form>
+    <p>
+      o nome do cara é {{first_name}} {{last_name}} e sua participação foi {{participation}}%
+    </p>
+  </div>
+
+  <h1>DATA</h1>
+  <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
+</template>
+<script>
+export default {
+  name: 'ParticipantForm',
+  data (){
+    return{
+      first_name: "",
+      last_name: "",
+      participation: null
+    }
+  },
+  methods: {
+    registerParticipant(e) {
+        e.preventDefault();
+        console.log(this.input_first_name);
+
+        this.first_name = this.input_first_name
+        this.last_name = this.input_last_name
+        this.participation = this.input_participation
+    }
+  }
+}
+</script>
